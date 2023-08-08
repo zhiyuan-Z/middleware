eff1dd886671bc157a7cce9a2910e78bc2ca2636
Zhiyuan Zhang <zhiyuan.zhang2000@outlook.com>
2023-08-07 19:54:36 -0500

    add redux-persist to save store in persistent storage

--- next-page-redux-saga/src/pages/_app.js
+++ next-page-redux-saga/src/pages/_app.js
@@ -1,8 +1,16 @@
-import { wrapper } from "@/store";
+import { persistor, wrapper } from "@/store";
 import "@/styles/globals.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <PersistGate
+      loading={<div>reconciling with local state</div>}
+      persistor={persistor}
+    >
+      <Component {...pageProps} />
+    </PersistGate>
+  );
 }
 
 export default wrapper.withRedux(App);
--- next-page-redux-saga/src/store/index.js
+++ next-page-redux-saga/src/store/index.js
@@ -5,31 +5,41 @@ import { all } from "redux-saga/effects";
 import listingsSagas from "./listings/listingsSagas";
 import { HYDRATE, createWrapper } from "next-redux-wrapper";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const appReducer = combineReducers({
   listings: listingsReducer,
 });
 
 const rootReducer = (state, action) => {
-  // reconciliate the hydrated state on top of the existing state
+  // reconcile the server-side state state on top of the existing state
   if (action.type === HYDRATE) {
     return { ...state, ...action.payload };
   }
   return appReducer(state, action);
 };
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// reconcile the persisted state in browser storage with existing state
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export function* rootSaga() {
   yield all([...listingsSagas]);
 }
 
-export const makeStore = context => {
-  const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(sagaMiddleware))
-  );
-  store.sagaTask = sagaMiddleware.run(rootSaga);
-  return store;
-};
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+store.sagaTask = sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
+export const makeStore = context => store;
 
 export const wrapper = createWrapper(makeStore, { debug: true });
